6f673af778684843f77b6e2e6428fbf2683275a0
Mr.KLeo <2315831906@qq.com>
2021-03-20 19:33:29 +0800

    我的页面

--- miniprogram/Yeo/YeoStampText/YeoStampText.js
+++ miniprogram/Yeo/YeoStampText/YeoStampText.js
@@ -4,7 +4,14 @@ Component({
    * 组件的属性列表
    */
   properties: {
+    // 邮票图片地址
+    stampPic: {
+      type: String,
+      value: 'https://s4.ax1x.com/2021/03/20/6h1kzn.png'
+    }
 
+
+    
   },
 
   /**
