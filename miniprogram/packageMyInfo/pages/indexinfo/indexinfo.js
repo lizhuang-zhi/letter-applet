08efa49cb5bb83d5c559e1f3f95209c757152c20
Mr.KLeo <2315831906@qq.com>
2021-03-20 17:24:14 +0800

    删除packageMyInfo子包

--- miniprogram/packageMyInfo/pages/indexinfo/indexinfo.js
+++ miniprogram/packageMyInfo/pages/indexinfo/indexinfo.js
@@ -1,12 +1,13 @@
+// packageMyInfo/pages/indexinfo/indexinfo.js
 // 1.这里的路径是相对于我的文件来说（开发者需要自行更改)
-let tools = require('../../Yeo/utils/tools');
+let tools = require('../../../Yeo/utils/tools');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    tabArray: ['公开日记', '吐槽大会']
+    tabArray: ['我的解忧', '公开日记', '吐槽大会']
   },
 
   // 2.初始化tab页面高度
@@ -24,6 +25,11 @@ Page({
       });
       tools.selectHeight('.tab-2').then(back => {
         tabHeiArr.push(back);
+      });
+      tools.selectHeight('.tab-3').then(back => {
+        tabHeiArr.push(back);
+        // 获取完成改变Promise状态
+        resolve('success');
       });
     }).then(res => {
       let newArr = tabHeiArr.sort((a, b) => a - b);
@@ -54,12 +60,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    if (typeof this.getTabBar === 'function' &&
-      this.getTabBar()) {
-      this.getTabBar().setData({
-        selected: 1
-      })
-    }
+
   },
 
   /**
