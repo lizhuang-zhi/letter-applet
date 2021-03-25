c2feb8156544cfba3e778f866482d711e13756b7
Mr.KLeo <2315831906@qq.com>
2021-03-25 17:50:04 +0800

    整改mailbox页面，新建replylist页面

--- miniprogram/pages/mailbox/mailbox.js
+++ miniprogram/pages/mailbox/mailbox.js
@@ -10,27 +10,75 @@ Page({
       text: "点赞",
       picBgColor: "#FBE9E8"
     }, {
-      imgsrc: "../../images/user.png",
-      text: "我的",
+      imgsrc: "https://z3.ax1x.com/2021/03/25/6OofiT.png",
+      text: "回信",
       picBgColor: "#E9F1FE"
     }, {
       imgsrc: "../../images/message.png",
       text: "评论",
       picBgColor: "#E6F8F0"
-    }]
+    }],
+
+    // loading组件
+    pull: {
+      isLoading: false,
+      loading: '../../images/loading-2.gif',
+      pullText: '正在加载'
+    },
+    push: {
+      isLoading: false,
+      loading: '../../images/loading-2.gif',
+      pullText: '-上拉加载更多-'
+    },
+    slideStart: [],
+    moveTime: 0,
   },
 
   // 跳转我的页面
-  ToMyInfoTap(e) {
-    // 获取点击索引
-    let index = e.detail.index;
-    console.log(index);
-    if(index == 1){
-      wx.navigateTo({
-        url: '/packageMyInfo/pages/indexinfo/indexinfo',
+  ToMyInfoTap() {
+    wx.navigateTo({
+      url: '/packageMyInfo/pages/indexinfo/indexinfo',
+    })
+
+  },
+
+  // 监听下拉刷新事件
+  refresh(e) {
+    console.log('刷新', e)
+    this.setData({
+      'pull.isLoading': true,
+      'pull.loading': '../../images/loading-2.gif',
+      'pull.pullText': '正在加载',
+    })
+    // setTimeout(() => {
+    //   this.setData({
+    //     'pull.loading': '../../images/loading-1.gif',
+    //     'pull.pullText': '刷新完成'
+    //   })
+    // }, 4000)
+    setTimeout(() => {
+      this.setData({
+        'pull.isLoading': false,
       })
-    }
-    
+      console.log('+++++ 刷新完成 +++++')
+    }, 6000)
+  },
+  // 监听上拉加载更多
+  toload(e) {
+    console.log('加载', e),
+      this.setData({
+        'push.isLoading': true,
+        'push.pullText': '正在加载',
+        'push.loading': '../../images/loading-2.gif',
+      })
+    setTimeout(() => {
+      this.setData({
+        'push.isLoading': false,
+        'push.pullText': '- 上拉加载更多 -',
+        'push.loading': '../../images/loading-2.gif',
+      })
+      console.log('===== 加载完成 =====')
+    }, 5000)
   },
 
   /**
