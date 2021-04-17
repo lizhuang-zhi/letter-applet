673cb3d1685d61ee0fe4e1f6312c9c8baaff518f
DESKTOP-SQ2LGJ3\HP <2315831906@qq.com>
2021-04-17 12:46:32 +0800

    开始设计第二版本图表

--- miniprogram/packageMyInfo/pages/indexinfo/indexinfo.js
+++ miniprogram/packageMyInfo/pages/indexinfo/indexinfo.js
@@ -1,10 +1,82 @@
-// packageMyInfo/pages/indexinfo/indexinfo.js
+import * as echarts from '../../ec-canvas/echarts';
+
+/* 
+  总数据显示
+*/
+let chart_all = null;
+let option_all = null;
+
+/* 切换的折线图 */
+function initChart(canvas, width, height) {
+  chart_all = echarts.init(canvas, null, {
+    width: width,
+    height: height
+  });
+  canvas.setChart(chart_all);
+
+  option_all = {
+    /* 图表类型说明 */
+    legend: {
+      top: 'bottom'
+    },
+    /* 点击模块提示 */
+    tooltip: {
+      trigger: 'item'
+    },
+    /* 模块元素 */
+    color: [
+      '#EE6666',
+      '#73C0DE',
+      '#FAC858'
+    ],
+    series: [{
+      name: '发布数量',
+      type: 'pie',
+      top: '-10%',
+      radius: [35, 105],
+      center: ['50%', '50%'],
+      roseType: 'area',
+      itemStyle: {
+        /* 微信小程序设置此圆角属性无效 */
+        borderRadius: 0,
+      },
+      data: [{
+          value: 4,
+          name: '解忧'
+        },
+        {
+          value: 2,
+          name: '日记'
+        },
+        {
+          value: 3,
+          name: '吐槽'
+        },
+      ],
+      /* 点击强调阴影颜色 */
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)'
+        }
+      }
+    }]
+  };
+
+  chart_all.setOption(option_all);
+  return chart_all;
+}
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    ec: {
+      onInit: initChart
+    }
   },
 
   /**
